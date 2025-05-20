eec4d6467ca522385c394c7578974bfbfd62a3e0
Tim Holy <tim.holy@gmail.com>
2025-05-20 15:04:02 -0500

    Add basic tests (#4)
    Also improves `linkpair` to display in the same window.

--- ext/CounterMarkingImageViewExt.jl
+++ ext/CounterMarkingImageViewExt.jl
@@ -6,23 +6,23 @@ using ImageSegmentation
 using ImageView
 using Random
 
-function colorize(seg, segidxs::AbstractSet{Int}, color::Colorant)
-    label = seg.image_indexmap
-    img = similar(label, promote_type(typeof(color), valtype(seg.segment_means)))
-    fill!(img, zero(eltype(img)))
-    for idx in eachindex(label)
-        label[idx] ∈ segidxs || continue
-        img[idx] = color
-    end
-    return img
-end
+# function colorize(seg, segidxs::AbstractSet{Int}, color::Colorant)
+#     label = seg.image_indexmap
+#     img = similar(label, promote_type(typeof(color), valtype(seg.segment_means)))
+#     fill!(img, zero(eltype(img)))
+#     for idx in eachindex(label)
+#         label[idx] ∈ segidxs || continue
+#         img[idx] = color
+#     end
+#     return img
+# end
 
 function linkpair(img, imgc)
-    gd = imshow(img)
-    zr = gd["roi"]["zoomregion"]
-    slicedata = gd["roi"]["slicedata"]
-    gdc = imshow(imgc, nothing, zr, slicedata)
-    return (gd, gdc)
+    zr, slicedata = roi(img)
+    gd = imshow_gui((800, 800), (2,1); slicedata=slicedata)
+    imshow(gd["frame"][1,1], gd["canvas"][1,1], img, nothing, zr, slicedata)
+    imshow(gd["frame"][2,1], gd["canvas"][2,1], imgc, nothing, zr, slicedata)
+    return gd
 end
 
 # For visualization
@@ -33,5 +33,8 @@ end
 
 CounterMarking.randshow(seg; kwargs...) = imshow(map(i->get_random_color(i), labels_map(seg)); kwargs...)
 CounterMarking.meanshow(seg; kwargs...) = imshow(map(i->segment_mean(seg, i), labels_map(seg)); kwargs...)
+
+CounterMarking.randshow(img, seg; kwargs...) = linkpair(img, map(i->get_random_color(i), labels_map(seg)); kwargs...)
+CounterMarking.meanshow(img, seg; kwargs...) = linkpair(img, map(i->segment_mean(seg, i), labels_map(seg)); kwargs...)
 
 end
